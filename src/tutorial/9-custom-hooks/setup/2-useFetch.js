d5a190d9994e750c3c1f8ebd3b495bb12ce01e4b
Frank Simon <f.m.sim@t-online.de>
2022-03-27 01:27:29 +0100

    added React.memo, useCallback and useMemo to products. added useCallback to fetch

--- src/tutorial/9-custom-hooks/setup/2-useFetch.js
+++ src/tutorial/9-custom-hooks/setup/2-useFetch.js
@@ -1,20 +1,20 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect, useCallback } from "react"
 
 // custom hooks have to begin with use...
 export const useFetch = url => {
   const [loading, setLoading] = useState(true)
   const [products, setProducts] = useState([])
 
-  const getProducts = async () => {
+  const getProducts = useCallback(async () => {
     const response = await fetch(url)
     const products = await response.json()
     setProducts(products)
     setLoading(false)
-  }
+  })
 
   useEffect(() => {
     getProducts()
-  }, [url])
+  }, [url, getProducts])
 
   return { loading, products }
 }
